af9d2d7499f869f98047414b37a858cb4da7da15
Ashutosh Sharma <kapil.shrm1.kks@gmail.com>
2022-06-22 22:47:38 +0530

    file update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from TrainingModel import trainModel
 from training_Validation_Insertion import train_validation
 #import flask_monitoringdashboard as dashboard
 from predictfrommodel import prediction
-
+from wsgiref import simple_server
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
