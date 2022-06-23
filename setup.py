4c836cfdd1c54a5579bcf59d748b3e285b2f0b24
Ashutosh Sharma <kapil.shrm1.kks@gmail.com>
2022-06-23 18:49:40 +0530

    file update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="credit-card-fault-detection",
     version="0.0.3",
     description="ML project",
-    author="Avnish yadav",
+    author="Ashutosh sharma",
     packages=find_packages(),
     license="MIT"
 )
